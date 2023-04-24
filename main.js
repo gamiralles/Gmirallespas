03c2aa776719d389a37c0c1a0300f66868d4d90f
gamiralles <gasti.miralles@gmail.com>
2023-04-24 19:20:09 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Ingrese su nombre"); //cliente
+let sueldo = parseInt(prompt(`Bienvenido ${nombre}, ingresa tu sueldo`)); //sueldo del cliente para evaluar el indice
+let alquiler = parseInt(prompt("Ingresa el valor del alquiler Mensual")); //valor del alquiler, para generar una garantia
+let indice = 2;
+let valorSeguro;
+
+
