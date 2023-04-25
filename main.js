a03a7cce380fcd1610f928c34b21522578d2617d
gamiralles <gasti.miralles@gmail.com>
2023-04-25 16:45:04 -0300

    First Commit

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-let nombre = prompt("Ingrese su nombre"); //cliente
-let sueldo = parseInt(prompt(`Bienvenido ${nombre}, ingresa tu sueldo`)); //sueldo del cliente para evaluar el indice
-let alquiler = parseInt(prompt("Ingresa el valor del alquiler Mensual")); //valor del alquiler, para generar una garantia
-let indice = 2;
-let valorSeguro;
+let nombre;
+let sueldo;
+let alquiler;
+let indice;
+
+do {
+  nombre = prompt("Ingrese su nombre o bien escriba fin para finalizar.");
+  if (nombre === "fin") break; // salir del ciclo si el usuario escribe "fin"
+  
+  sueldo = parseInt(prompt(`Bienvenido ${nombre}, ingrese su sueldo`));
+
+  if (sueldo <= 100000) {
+    indice = 1.6;
+  } else if (sueldo > 100000 && sueldo <= 150000) {
+    indice = 1.8;
+  } else {
+    indice = 2;
+  }
+
+  do {
+    alquiler = parseInt(prompt("Ingresa el valor del alquiler mensual"));
+
+    if (alquiler <= 0) {
+      alert("El valor del alquiler debe ser mayor a cero. Por favor, ingrese un valor válido.");
+    }
+  } while (alquiler <= 0);
+
+  function valorSeguro(alquiler, indice) {
+    return alquiler / indice;
+  }
+
+  alert(`El valor del seguro es de: ${valorSeguro(alquiler, indice)} pesos anuales. Gracias`);
+
+} while (nombre != "fin");
 
 
