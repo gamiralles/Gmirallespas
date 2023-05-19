75d057655f395ad1a8a1565542913e6c0ae6eebd
gamiralles <gasti.miralles@gmail.com>
2023-05-19 14:20:53 -0300

    segunda PreEntrega

--- main.js
+++ main.js
@@ -1,36 +1,101 @@
-let nombre;
+let numeroCliente;
 let sueldo;
 let alquiler;
-let indice;
+let valorSeguro;
 
-do {
-  nombre = prompt("Ingrese su nombre o bien escriba fin para finalizar.");
-  if (nombre === "fin") break; // salir del ciclo si el usuario escribe "fin"
-  
-  sueldo = parseInt(prompt(`Bienvenido ${nombre}, ingrese su sueldo`));
+class datosClientes {
+  constructor(nombre, edad, dni, telefono, email) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+    this.telefono = telefono;
+    this.email = email;
+  }
+}
 
-  if (sueldo <= 100000) {
-    indice = 1.6;
+const gastonMiralles = new datosClientes(
+  "Gaston Miralles",
+  "29",
+  "38089331",
+  "1154879865",
+  "gmiralles@gseguros.com.ar"
+);
+
+const javierGonzalez = new datosClientes(
+  "Javier Gonzalez",
+  "31",
+  "38089332",
+  "1154879866",
+  "jgonzalez@gseguros.com.ar"
+);
+
+const martinFernandez = new datosClientes(
+  "Martin Fernandez",
+  "25",
+  "38089333",
+  "1154879864",
+  "mfernandez@gseguros.com.ar"
+);
+
+const arrayClientes = [];
+
+arrayClientes.push(gastonMiralles);
+arrayClientes.push(javierGonzalez);
+arrayClientes.push(martinFernandez);
+
+function numCliente() {
+  numeroCliente = prompt(`Ingrese su dni:`);
+  return numeroCliente;
+}
+
+function nuevoCliente() {
+  let nombre = prompt("Ingrese su Nombre y Apellido:");
+  let edad = parseInt(prompt("Ingrese su edad:"));
+  let dni = numeroCliente;
+  let telefono = parseInt(prompt("Ingrese su Telefono:"));
+  let email = prompt("Ingrese su Email:");
+  let cliente = new datosClientes(nombre, edad, dni, telefono, email);
+  arrayClientes.push(cliente);
+  console.log(arrayClientes);
+}
+
+function cotizador() {
+  sueldo = parseInt(prompt("Por favor ingrese su remuneracion mensual."));
+  alquiler = parseInt(
+    prompt("Por favor ingrese el valor del alquiler mensual.")
+  );
+
+  if (sueldo >= 200000) {
+    valorSeguro = alquiler / 2;
   } else if (sueldo > 100000 && sueldo <= 150000) {
-    indice = 1.8;
+    valorSeguro = alquiler / 1.8;
   } else {
-    indice = 2;
+    valorSeguro = alquiler / 1.6;
   }
 
+  alert(`El valor del seguro es de $ ${valorSeguro} mensuales.`);
+}
+
+function inicio() {
+  let respuesta;
   do {
-    alquiler = parseInt(prompt("Ingresa el valor del alquiler mensual"));
-
-    if (alquiler <= 0) {
-      alert("El valor del alquiler debe ser mayor a cero. Por favor, ingrese un valor válido.");
+    alert(`Bienvenido/a al cotizador de caución.`);
+    numCliente();
+    if (arrayClientes.some((cliente) => cliente.dni === numeroCliente)) {
+      alert(`Usted ya es Cliente.`);
+      cotizador();
+      console.log(arrayClientes);
+    } else {
+      alert(`Por favor ingrese sus datos.`);
+      nuevoCliente();
+      alert(`Bienvenido a Gaston Miralles productor asesor de Seguros.`);      
+      cotizador();
+      console.log(arrayClientes);
     }
-  } while (alquiler <= 0);
+    respuesta = prompt(
+      "¿Desea realizar otra cotización? si/no:"
+    );
+  } while (respuesta.toLowerCase() === "si");
+}
 
-  function valorSeguro(alquiler, indice) {
-    return alquiler / indice;
-  }
-
-  alert(`El valor del seguro es de: ${valorSeguro(alquiler, indice)} pesos anuales. Gracias`);
-
-} while (nombre != "fin");
-
-
+inicio()
